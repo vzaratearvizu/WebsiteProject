48152e9fd224db0d553eb65e3def20dc37b248dd
vzaratearvizu <zaratearvizuv@gmail.com>
2024-12-16 21:18:24 -0500

    LearningVariables
    A little bit about variables. No idea as to what's to come next.

---
+++ ExploringVisualCodeJavaScript.js
@@ -0,0 +1,38 @@
+/* Press 'ctrl shift p' to open up command palette,
+then type 'theme' to select a GitHub Desktop color theme.
+Either 'ctrl s' or turn on autosave.
+Press 'F5' to run code. It's a shortcut. */
+
+// This is how to make comments in JavaScript
+
+/* This is for
+more than
+one line */
+
+// Create variables with 'let' and 'const'
+// Codes ends in ';' in JavaScript
+let forever1 = 1;
+let foreverOne = "One"; 
+const forever2 = 2;
+const foreverTwo = "Two";
+
+// 'const' is suppose to forever remain the same
+const dontChange = "This value should remain the same no matter what and an error happens when it tries to be changed";
+
+// 'let' allows variablesd to be changed
+let mentalHealth = "high";
+mentalHealth = ":(";
+
+// I think there's some convention where 'const' is all caps. Multiple words are written differently too.
+const ALLCAPS = "this string is?";
+const ALL_CAPS = "this string is!";
+
+// By default, we should define stuff with 'const' instead of using 'let'
+
+// camelCase and snake_case are different
+let camelCaseIGuess = "first Word Is Lowercase";
+const SNAKE_CASE_I_GUESS = "EVERYTHING_IS_CAPPED_PLUS_THAT_UNDERSCORE_SYMBOL_INBETWEEN";
+
+// 'console.log' can output variables into the, well, console  ._.
+console.log("stringy string, string string :)")
+console.log(camelCaseIGuess)
